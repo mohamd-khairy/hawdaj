f96e4c1150ab11bf2967465557ecf984ce293a75
ahmadbayrakdar <ahmad.berkdar.sy@gmail.com>
2022-11-30 23:49:09 +0200

    added sections for vectors

--- resources/views/front/trip/make_trip.blade.php
+++ resources/views/front/trip/make_trip.blade.php
@@ -5,12 +5,12 @@
         position: fixed;
         top: 0;
         left: 0;
-        width: 40%;
+        width: 70%;
         /* height: calc(100% - 100px); */
         background-color: #fff;
         z-index: 4;
-        margin-top: 100px;
-        margin-left: 30%;
+        margin-top: 75px;
+        margin-left: 15%;
         border-radius: 10px;
         box-shadow: 0 0 10px #000;
         padding: 25px 0 0;
@@ -30,27 +30,25 @@
     .popup_that_shows_on_startup #make_a_trip_popup > h5{
         margin-bottom: 20px;
         font-weight: bold;
+        width: 60%;
     }
 
     .popup_that_shows_on_startup #make_a_trip_popup .tabs{
         flex-wrap: nowrap;
     }
 
-    .popup_that_shows_on_startup #make_a_trip_popup .tabs > div{
-        padding-bottom: 25px;
-    }
-
     .popup_that_shows_on_startup #make_a_trip_popup .main_tab, .popup_that_shows_on_startup #make_a_trip_popup .tab{
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         justify-content: space-evenly;
     }
 
-    .popup_that_shows_on_startup #make_a_trip_popup .main_tab > p.description{
-        margin: 20px;
+    .popup_that_shows_on_startup #make_a_trip_popup .main_tab > .left-side > p.description{
+        width: 75%;
+        margin: 20px auto;
     }
 
-    .popup_that_shows_on_startup #make_a_trip_popup .main_tab > button.make_a_trip_button{
+    .popup_that_shows_on_startup #make_a_trip_popup .main_tab > .left-side > button.make_a_trip_button{
         border-radius: 50px;
         padding: 10px 20px;
         font-weight: bold;
@@ -62,9 +60,27 @@
         border-color: #2c085d;
     }
 
-    .popup_that_shows_on_startup #make_a_trip_popup .main_tab > button.make_a_trip_button:hover:not([disabled]),
+    .popup_that_shows_on_startup .tabs .left-side{
+        padding-bottom: 25px;
+        width: 60%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+    
+    .popup_that_shows_on_startup .tabs .right-side{
+        width: 40%;
+    }
+    
+    .popup_that_shows_on_startup .tabs .right-side img{
+        height: 75%;
+        width: 85%;
+        object-fit: contain;
+    }
+
+    .popup_that_shows_on_startup #make_a_trip_popup .main_tab > .left-side > button.make_a_trip_button:hover:not([disabled]),
     .popup_that_shows_on_startup #make_a_trip_popup .tab button.make_a_trip_button:hover:not([disabled]),
-    .popup_that_shows_on_startup .tab > button:hover:not([disabled]){
+    .popup_that_shows_on_startup .tab > .left-side > button:hover:not([disabled]){
         background-color: #2c085d;
         color: #fff;
     }
@@ -99,7 +115,7 @@
         width: 100%;
     }
 
-    .popup_that_shows_on_startup .tab > *:not(h6):not(.trip_categories):not(a):not(.navigation_buttons), #make_a_trip_popup .tab .trip_categories > span{
+    .popup_that_shows_on_startup .tab > .left-side > *:not(h6):not(.trip_categories):not(a):not(.navigation_buttons), #make_a_trip_popup .tab .trip_categories > span{
         width: 70%;
         margin: 5px auto;
         height: 40px;
@@ -109,18 +125,18 @@
         cursor: pointer;
     }
 
-    .popup_that_shows_on_startup .tab > select#categories, .popup_that_shows_on_startup .tab > select#key_words{
+    .popup_that_shows_on_startup .tab > .left-side > select#categories, .popup_that_shows_on_startup .tab > .left-side > select#key_words{
         max-height: 75px;
     }
 
-    .popup_that_shows_on_startup .tab > button#login, .popup_that_shows_on_startup .tab > button#register{
+    .popup_that_shows_on_startup .tab > .left-side > button#login, .popup_that_shows_on_startup .tab > .left-side > button#register{
         border: solid 2px #000;
         font-weight: bold;
         border-radius: 50px;
         font-weight: bold;
     }
 
-    .popup_that_shows_on_startup .tab > .navigation_buttons{
+    .popup_that_shows_on_startup .tab > .left-side > .navigation_buttons{
         width: 70%;
         margin: 5px auto;
         height: 40px;
@@ -145,7 +161,7 @@
         transition: all 0.2s;
     }
 
-    .popup_that_shows_on_startup .tab > input{
+    .popup_that_shows_on_startup .tab > .left-side > input{
         cursor: auto !important;
     }
 
@@ -170,103 +186,132 @@
             @csrf
             <div class="tabs row">
                 <div  dir="{{(app()->getLocale() === 'en') ? 'ltr' : 'rtl'}}" class="main_tab col-12">
-                    <p class="description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo qui incidunt, quam id architecto, repellat vel fuga placeat distinctio quia magni praesentium reiciendis aspernatur eveniet iure? Omnis dicta eligendi tempore.
-                    </p>
-                    <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(1)">{{__('dashboard.make_a_trip')}}</button>
+                    <div class="left-side">
+                        <p class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo qui incidunt, quam id architecto, repellat vel fuga placeat distinctio quia magni praesentium reiciendis aspernatur eveniet iure? Omnis dicta eligendi tempore.
+                        </p>
+                        <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(1)">{{__('dashboard.make_a_trip')}}</button>
+                        </div>
+                    <div class="right-side">
+                        <img src="{{ asset('front_assets/imgs/popup_images/popup_1.png') }}" alt="fabulous trip image">
+                    </div>
                 </div>
 
                 <div  dir="{{(app()->getLocale() === 'en') ? 'ltr' : 'rtl'}}" class="tab hide col-12">
-                    <h6>Select Date and Place</h6>
-                    <input type="date" name="date" placeholder="date" id="date" required>
-                    <select name="region_id" id="region_id"  required>
-                        <option value="">{{ __('dashboard.select_region') }}</option>
-                        @foreach($regions as $region)
-                        <option value="{{ $region->id }}" {{ old('region_id') == $region->id ? 'selected ': '' }}>{{ $region->name ?? '---' }}</option>
-                        @endforeach
-                    </select>
-                    <select name="city_id" id="city_id">
-                        <option value="">{{ __('dashboard.select_city') }}</option>
-                    </select>
-                    <div class="navigation_buttons">
-                        <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(0)">{{__('dashboard.back')}}</button>
-                        <button type="button" class="make_a_trip_button next" onclick="makeATripNextTab(2)" disabled>{{__('dashboard.next')}}</button>
+                    <div class="left-side">
+                        <h6>Select Date and Place</h6>
+                        <input type="date" name="date" placeholder="date" id="date" required>
+                        <select name="region_id" id="region_id"  required>
+                            <option value="">{{ __('dashboard.select_region') }}</option>
+                            @foreach($regions as $region)
+                            <option value="{{ $region->id }}" {{ old('region_id') == $region->id ? 'selected ': '' }}>{{ $region->name ?? '---' }}</option>
+                            @endforeach
+                        </select>
+                        <select name="city_id" id="city_id">
+                            <option value="">{{ __('dashboard.select_city') }}</option>
+                        </select>
+                        <div class="navigation_buttons">
+                            <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(0)">{{__('dashboard.back')}}</button>
+                            <button type="button" class="make_a_trip_button next" onclick="makeATripNextTab(2)" disabled>{{__('dashboard.next')}}</button>
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <img src="{{ asset('front_assets/imgs/popup_images/popup_1.png') }}" alt="fabulous trip image">
                     </div>
                 </div>
                 <div  dir="{{(app()->getLocale() === 'en') ? 'ltr' : 'rtl'}}" class="tab hide col-12">
-                    <h6>Specify Your Desired Trip</h6>
-                    <select name="season" id="season" required>
-                        @foreach($seasons as $item)
-                        <option value="{{$item}}">{{$item}}</option>
-                        @endforeach
-                    </select>
+                    <div class="left-side">
+                        <h6>Specify Your Desired Trip</h6>
+                        <select name="season" id="season" required>
+                            @foreach($seasons as $item)
+                            <option value="{{$item}}">{{$item}}</option>
+                            @endforeach
+                        </select>
 
-                    <select name="price" id="price" required>
-                        @foreach($prices as $item)
-                        <option value="{{$item->id}}">{{$item->name}}</option>
-                        @endforeach
-                    </select>
-                    <input type="number" name="days" placeholder="days" id="days" required>
-                    <input type="number" name="funny_days" placeholder="funny days" id="funny_days" required>
-                    <div class="navigation_buttons">
-                        <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(1)">{{__('dashboard.back')}}</button>
-                        <button type="button" class="make_a_trip_button next" onclick="makeATripNextTab(3)" disabled>{{__('dashboard.next')}}</button>
+                        <select name="price" id="price" required>
+                            @foreach($prices as $item)
+                            <option value="{{$item->id}}">{{$item->name}}</option>
+                            @endforeach
+                        </select>
+                        <input type="number" name="days" placeholder="days" id="days" required>
+                        <input type="number" name="funny_days" placeholder="funny days" id="funny_days" required>
+                        <div class="navigation_buttons">
+                            <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(1)">{{__('dashboard.back')}}</button>
+                            <button type="button" class="make_a_trip_button next" onclick="makeATripNextTab(3)" disabled>{{__('dashboard.next')}}</button>
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <img src="{{ asset('front_assets/imgs/popup_images/popup_1.png') }}" alt="fabulous trip image">
                     </div>
                 </div>
                 <div  dir="{{(app()->getLocale() === 'en') ? 'ltr' : 'rtl'}}" class="tab hide col-12">
+                    <div class="left-side">
+                        <h6>Select as Many as You Like</h6>
 
-                    <h6>Select as Many as You Like</h6>
+                        <select style="height: 150px;" name="key_words[]" id="key_words" multiple>
+                            @foreach($key_words as $item)
+                            <option value="{{$item}}">{{$item}}</option>
+                            @endforeach
+                        </select>
 
-                    <select style="height: 150px;" name="key_words[]" id="key_words" multiple>
-                        @foreach($key_words as $item)
-                        <option value="{{$item}}">{{$item}}</option>
-                        @endforeach
-                    </select>
+                        <h6>Select as Many as You Like</h6>
 
-                    <h6>Select as Many as You Like</h6>
+                        <select style="height: 150px;" name="categories[]" id="categories" multiple>
+                            @foreach($categories as $item)
+                            <option value="{{$item->id}}">{{$item->name}}</option>
+                            @endforeach
+                        </select>
 
-                    <select style="height: 150px;" name="categories[]" id="categories" multiple>
-                        @foreach($categories as $item)
-                        <option value="{{$item->id}}">{{$item->name}}</option>
-                        @endforeach
-                    </select>
-
-                    <div class="navigation_buttons">
-                        <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(2)">{{__('dashboard.back')}}</button>
-                        <button type="button" class="make_a_trip_button next" onclick="makeATripNextTab(4)">{{__('dashboard.next')}}</button>
+                        <div class="navigation_buttons">
+                            <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(2)">{{__('dashboard.back')}}</button>
+                            <button type="button" class="make_a_trip_button next" onclick="makeATripNextTab(4)">{{__('dashboard.next')}}</button>
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <img src="{{ asset('front_assets/imgs/popup_images/popup_1.png') }}" alt="fabulous trip image">
                     </div>
                 </div>
                 <div  dir="{{(app()->getLocale() === 'en') ? 'ltr' : 'rtl'}}" class="tab hide col-12">
-                    @if(!auth()->check())
+                    <div class="left-side">
+                        @if(!auth()->check())
 
                     
-                    <h6>Log In</h6>
-                    <input type="email" name="email" placeholder="Email" required>
-                    <input type="password" name="password" placeholder="Password" required>
-                    <button type="button" id="login" disabled>{{__('dashboard.login')}}</button>
-                    <button type="submit" id="as_a_guest" class="">{{__('dashboard.continueAsAguest')}}</button>
+                        <h6>Log In</h6>
+                        <input type="email" name="email" placeholder="Email" required>
+                        <input type="password" name="password" placeholder="Password" required>
+                        <button type="button" id="login" disabled>{{__('dashboard.login')}}</button>
+                        <button type="submit" id="as_a_guest" class="">{{__('dashboard.continueAsAguest')}}</button>
 
-                    <a href="#" onclick="makeATripNextTab(5)">{{__('dashboard.register')}}</a>
-                    @else
-                    <button type="submit" class="">{{__('dashboard.continue')}}</button>
-                    @endif
+                        <a href="#" onclick="makeATripNextTab(5)">{{__('dashboard.register')}}</a>
+                        @else
+                        <button type="submit" class="">{{__('dashboard.continue')}}</button>
+                        @endif
 
-                    <div class="navigation_buttons">
-                        <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(3)">{{__('dashboard.back')}}</button>
+                        <div class="navigation_buttons">
+                            <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(3)">{{__('dashboard.back')}}</button>
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <img src="{{ asset('front_assets/imgs/popup_images/popup_1.png') }}" alt="fabulous trip image">
                     </div>
                 </div>
 
                 @if(!auth()->check())
                 <div class="tab hide col-12">
-                    <h6>register </h6>
-                    <input type="text" name="first_name" placeholder="First Name" required>
-                    <input type="text" name="last_name" placeholder="Last Name" required>
-                    <input type="email" name="register_email" placeholder="Email" required>
-                    <input type="password" name="register_password" placeholder="Password" required>
-                    <button type="button" id="register" disabled>{{__('dashboard.register')}}</button>
+                    <div class="left-side">
+                        <h6>register </h6>
+                        <input type="text" name="first_name" placeholder="First Name" required>
+                        <input type="text" name="last_name" placeholder="Last Name" required>
+                        <input type="email" name="register_email" placeholder="Email" required>
+                        <input type="password" name="register_password" placeholder="Password" required>
+                        <button type="button" id="register" disabled>{{__('dashboard.register')}}</button>
 
-                    <div class="navigation_buttons">
-                        <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(4)">{{__('dashboard.back')}}</button>
+                        <div class="navigation_buttons">
+                            <button type="button" class="make_a_trip_button" onclick="makeATripNextTab(4)">{{__('dashboard.back')}}</button>
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <img src="{{ asset('front_assets/imgs/popup_images/popup_1.png') }}" alt="fabulous trip image">
                     </div>
                 </div>
                 @endif
